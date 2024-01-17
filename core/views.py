9a250522ad2ee7f46013c4afd4da3de89e35616a
npaudyal321 <naween321@gmail.com>
2024-01-17 13:27:31 +0545

    context and temp inheritance

--- core/views.py
+++ core/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def students(request):
-    return render(request, template_name='core/students.html')
+    stds = [
+        {"name": "Jon", "email": "j@email.com", "age": 30, "address": "KTM"},
+        {"name": "Jane", "email": "jane@email.com", "age": 30, "address": "KTM"},
+        {"name": "Hary", "email": "hary@email.com", "age": 30, "address": "KTM"},
+        {"name": "Alex", "email": "alex@email.com", "age": 30, "address": "KTM"},
+    ]
+    return render(request, template_name='core/students.html',
+                  context={"students": stds, "title": "Students"})
