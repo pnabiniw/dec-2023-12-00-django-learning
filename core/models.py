0a2a2b33f5ba7c1fce9b83246069eac9d21b4380
npaudyal321 <naween321@gmail.com>
2024-01-18 13:25:52 +0545

    ORM

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    email = models.EmailField(max_length=20)
+    address = models.CharField(max_length=20)
--- core/views.py
+++ core/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
+from .models import Student
 
 
 def students(request):
-    stds = [
-        {"name": "Jon", "email": "j@email.com", "age": 30, "address": "KTM"},
-        {"name": "Jane", "email": "jane@email.com", "age": 30, "address": "KTM"},
-        {"name": "Hary", "email": "hary@email.com", "age": 30, "address": "KTM"},
-        {"name": "Alex", "email": "alex@email.com", "age": 30, "address": "KTM"},
-    ]
+    stds = Student.objects.all()
     return render(request, template_name='core/students.html',
                   context={"students": stds, "title": "Students"})
