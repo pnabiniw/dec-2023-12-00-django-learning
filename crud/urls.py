27c2eb8f443efc91df767d81cf600d1b28a7cf72
npaudyal321 <naween321@gmail.com>
2024-01-21 13:18:24 +0545

    student crud

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import classroom, delete_classroom
+from .views import classroom, delete_classroom, student, delete_student
 
 
 urlpatterns = [
     path('classroom/', classroom, name='classroom'),
+    path('student/', student, name='crud_student'),
     path('delete-classroom/<int:id>/', delete_classroom, name='delete_classroom'),
+    path('delete-student/<int:id>/', delete_student, name='delete_student'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import ClassRoom
+from .models import ClassRoom, Student
 
 
 def classroom(request):
@@ -17,3 +17,26 @@ def delete_classroom(request, id):
         clroom.delete()
         return redirect('classroom')
     return render(request, template_name='crud/delete_classroom.html', context={"classroom": clroom})
+
+
+def student(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        age = request.POST.get("age")
+        address = request.POST.get("address")
+        classroom_id = request.POST.get("classroom")
+        Student.objects.create(name=name, email=email, age=age, address=address, classroom_id=classroom_id)
+        return redirect('crud_student')
+    students = Student.objects.all()
+    classrooms = ClassRoom.objects.all()
+    return render(request, template_name='crud/student.html',
+                  context={"students": students, "title": "Students", "classrooms": classrooms})
+
+
+def delete_student(request, id):
+    student = Student.objects.get(id=id)
+    if request.method == "POST":
+        student.delete()
+        return redirect('crud_student')
+    return render(request, template_name="crud/delete_student.html", context={"student": student})
